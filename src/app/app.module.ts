3cbf6259fd1713b68983602f1245972ad8b69014
Luciano Murruni <luciano.murruni@conte.it>
2019-04-10 17:49:07 +0200

    first implementation of movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+import { IosInstallComponent } from './ios-install/ios-install.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    IosInstallComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  omdbApiUrl: 'https://www.omdbapi.com/?s=',
+  omdbApiApikey: 'fee8d867' // http://www.omdbapi.com/,
+
 };
 
 /*
